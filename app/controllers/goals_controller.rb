5d19dabf7d413aa7eccf3f638ba7ca615d4c23b9
Jesse Silliman <jksilliman@gmail.com>
2011-10-16 15:58:05 -0500

    Fixing phone

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -59,7 +59,7 @@ class GoalsController < ApplicationController
   def phone
     require 'contact'
     current_user.update_attribute(:phone_number, params[:phone_number])
-    Contact(current_user.phone_number, params[:message])
+    Contact.sms(current_user.phone_number, params[:message])
     redirect_to :goals
   end
 
